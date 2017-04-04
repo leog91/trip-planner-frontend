68ca9106983d312a2a33d27dd7401f71c3d2b73a
leog <leog91@gmail.com>
2017-04-04 19:15:14 -0300

    base proyect

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tripplannerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tripplannerApp
+ */
+angular.module('tripplannerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tripplannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tripplannerApp
+ */
+angular.module('tripplannerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
